419840ebd2d0d65683923ac0f2eb3fea5a7d4d7b
isabelyb <isabelcristinayepesb@gmail.com>
2021-10-10 16:31:49 -0500

    decorator challenge url

--- word_counter_url.py
+++ word_counter_url.py
@@ -6,7 +6,7 @@ def word_counter(func):
     def counter(*args, **kwargs):
         data = func(*args, **kwargs)    
         total_words = data.split()
-        stopwords = ['y','Y','la','de','una','los', 'me', 'No', 'con', 'que']
+        stopwords = ['y','Y','la','de','una','los','me','No','con','que','el','un','es','en','Que','muy','al','a','él','le','quiere','A']
         words = [word for word in total_words if word not in stopwords]
         wordcount = Counter(words)
         print('\n>>> TOP FIVE WORDS IN THIS SONG <<<\n')
@@ -23,8 +23,9 @@ def text_reader(url):
 
 
 chilanga_banda = 'https://raw.githubusercontent.com/isabelyb/word_counter/main/chilanga_banda_lyrics.txt'
-url2 = "/home/isa/estudio/Python/python_basic/09_professional_course/word_counter/chilanga_banda_lyrics.txt"
-url3 = "/home/isa/estudio/Python/python_basic/09_professional_course/08_chilanga_banda_lyrics.txt"
+p_to = 'https://raw.githubusercontent.com/isabelyb/word_counter/main/pto.txt'
+cambalache = 'https://raw.githubusercontent.com/isabelyb/word_counter/main/cambalache.txt'
 
 text_reader(chilanga_banda)
-
+text_reader(p_to)
+text_reader(cambalache)
